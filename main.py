7f95961ca31331bba0280fffef142a6a4dc8e5e5
Evan Woods <22337004+efwoods@users.noreply.github.com>
2024-05-15 13:43:11 -0400

    update

--- main.py
+++ main.py
@@ -1,3 +1,19 @@
 import pyautogui
-pyautogui.locateOnScreen('/Users/evanwoods/github/pyautogui-webgrid/images/blue-button.png', confidence=0.9)
+import os
 
+def clickWebGridTarget():
+    cwd = os.getcwd()
+    target_image = cwd + '/images/blue-button.png'
+    test_logo = cwd + '/images/logo.png'
+
+    target = pyautogui.locateCenterOnScreen(target_image, confidence=0.9)
+    pyautogui.click(target[0], target[1])
+
+def saveScreenshot():
+    im = pyautogui.screenshot('test_screenshot.png')
+
+def helloWorld():
+    print('hello world')
+
+if __name__ == '__main__':
+    saveScreenshot()
